c61c55a4bd1f7519f9b5389d99e50afddfb17235
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-07 17:28:59 +0300

    Added another InputContainer in App to create list section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import {v4 as uuid} from "uuid";
 import "./App.css";
+import InputContainer from "./components/input/InputContainer";
 import List from "./components/List/List";
 import store from "./utils/store";
 import StoreApi from "./utils/storeApi";
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyle = makeStyles((theme)=> ({
+  root:{
+    display: "flex",
+    minHeight: "100vh",
+    background: "cadetblue"
+  },
+}))
+
+
 
 function App() {
+
+  const classes = useStyle()
   const [data, setData] = useState(store);
   const addMoreCard = (title,listId) => {
       const newCardId = uuid();
@@ -29,13 +44,16 @@ function App() {
       setData(newState);
   }
 
+
   return (
     <StoreApi.Provider value= {{addMoreCard}}>
-      <div className="App">
+      <div className={classes.root}>
         {data.listIds.map((listId) => {
           const list = data.lists[listId];
           return <List list={list} key={listId} />;
         })}
+
+        <InputContainer type="list"/>
       </div>
     </StoreApi.Provider>
   );

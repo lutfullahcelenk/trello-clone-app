9ead13274dfb5b4442c33249063faf957901e3c2
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-08 09:34:22 +0300

    Droppable is added and taken inside a fucnt.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,35 +1,46 @@
-import React from 'react';
-import { CssBaseline, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
-import Title from './Title';
-import Card from './Card';
-import InputContainer from '../input/InputContainer';
+import React from "react";
+import { CssBaseline, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import Title from "./Title";
+import Card from "./Card";
+import InputContainer from "../input/InputContainer";
+import { Droppable } from "react-beautiful-dnd";
 
 const useStyle = makeStyles((theme) => ({
-    //theme's spacing default value : 8px
-    root: {
-        width: "300px",
-        backgroundColor: "lightgray",
-        color: "blue",
-        marginLeft: theme.spacing(2)      
-    },
+  //theme's spacing default value : 8px
+  root: {
+    width: "300px",
+    backgroundColor: "lightgray",
+    color: "blue",
+    marginLeft: theme.spacing(2),
+  },
 
-}))
+  cardContainer:{
+      marginTop : theme.spacing(4)
+  }
+}));
 
-const List = ({list}) => {
-    const classes = useStyle();
-    return (
-        <div>
-            <Paper className = {classes.root}>
-                <CssBaseline/>
-                <Title title = {list.title} listId={list.id} />
-                {list.cards.map((card) =>(
-                    <Card key={card.id}  card = {card} />
-                ))}
-                <InputContainer listId = {list.id} type = "card"/>
-            </Paper>
-        </div>
-    )
-}
+const List = ({ list }) => {
+  const classes = useStyle();
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <CssBaseline />
+        <Title title={list.title} listId={list.id} />
+        <Droppable droppableId={list.id}>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps} className= {classes.cardContainer}  >
+              {list.cards.map((card,index) => (
+                <Card key={card.id} card={card} index={index} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <InputContainer listId={list.id} type="card" />
+      </Paper>
+    </div>
+  );
+};
 
 export default List;

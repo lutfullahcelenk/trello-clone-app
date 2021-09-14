cbc3088f7cea5c89b429f2c7685a52a455d43f3a
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-14 09:09:00 +0300

    matirul ui card is applied

---
+++ src/components/TrelloCard.js
@@ -0,0 +1,14 @@
+import { Card, Typography } from "@material-ui/core";
+import React from "react";
+
+const TrelloCard = () => {
+  return (
+    <div>
+      <Card>
+        <Typography gutterBottom>Word of the Day</Typography>
+      </Card>
+    </div>
+  );
+};
+
+export default TrelloCard;

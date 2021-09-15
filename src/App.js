308b811a46fa7c1b93f97ac589e26354fd4f6f67
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-15 12:47:00 +0300

    Drag and Drop context is added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,58 @@ import "./App.css";
 import TrelloList from "./components/TrelloList";
 import { useSelector } from "react-redux";
 import TrelloActionButton from "./components/TrelloActionButton";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { useDispatch } from "react-redux";
+import { sort } from "./actions/listsActions";
 
 const App = () => {
-  const lists = useSelector((state) => state.lists);
-  // console.log(lists);
+  const myLists = useSelector((state) => state.lists);
+  // console.log(myLists);
+  const dispatch = useDispatch();
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result;
+    if (!destination) {
+      return;
+    }
+    dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId,
+        type
+      )
+    );
+  };
 
   return (
-    <div>
-      <h1>Hello</h1>
-      <div style={{ display: "flex", marginRight: "8px" }}>
-        {lists.map((list) => (
-          <TrelloList
-          title={list.title}
-          cards={list.cards}
-          key={list.id}
-          listId = {list.id}
-          />
-        ))}
-        <TrelloActionButton type="list" />
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="all-lists" direction="horizontal" type="list">
+          {(provided) => (
+            <div
+              style={{ display: "flex", marginRight: 8 }}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {myLists.map((list, index) => (
+                <TrelloList
+                  key={list.id}
+                  listId={list.id}
+                  title={list.title}
+                  cards={list.cards}
+                  index={index}
+                />
+              ))}
+              <TrelloActionButton type="list" />
+            </div>
+          )}
+        </Droppable>
       </div>
-    </div>
+    </DragDropContext>
   );
-}
+};
 
 export default App;

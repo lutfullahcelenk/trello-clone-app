29a614bb5df2707cc2b7bb3a15a6d2d376597c54
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-15 12:47:30 +0300

    It is both dragable and droppable, so we put the two func. here

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,37 +1,57 @@
-import { makeStyles } from '@material-ui/core';
-import React from 'react';
-import TrelloActionButton from './TrelloActionButton';
-import TrelloCard from './TrelloCard';
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import TrelloActionButton from "./TrelloActionButton";
+import TrelloCard from "./TrelloCard";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: "#dfe3e6",
+    borderRadius: 3,
+    width: 300,
+    padding: 8,
+    height: "100%",
+    margin: "0 20px",
+  },
+});
 
+const TrelloList = ({ title, cards, listId, index }) => {
+  const classes = useStyles();
 
-const useStyles = makeStyles ({
-    container: {
-        backgroundColor: "#dfe3e6",
-        borderRadius : 3,
-        width: 300,
-        padding: 8,
-        height: "100%",
-        margin: "0 20px",
-    },
-    
-})
+  return (
+    <Draggable draggableId={String(listId)} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+        >
+          <Droppable droppableId={String(listId)} type="card">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={classes.container}
+              >
+                <h3>{title}</h3>
 
-const TrelloList = ({title , cards , listId}) => {
-
-    const classes = useStyles();
-
-    return (
-        <div className={classes.container}>
-
-            <h3>{title}</h3>
-
-            {cards.map(card=> <TrelloCard text={card.text} key={card.id} />)}
-
-            <TrelloActionButton type="card" listId = {listId} />
-
+                {cards.map((card, index) => (
+                  <TrelloCard
+                    text={card.text}
+                    index={index}
+                    key={card.id}
+                    id={card.id}
+                  />
+                ))}
+                {provided.placeholder}
+                <TrelloActionButton listId={listId} type="card" />
+              </div>
+            )}
+          </Droppable>
         </div>
-    )
-}
+      )}
+    </Draggable>
+  );
+};
 
-export default TrelloList
+export default TrelloList;

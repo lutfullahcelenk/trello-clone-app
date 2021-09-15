f9a78b9db968b0cfce3e5fbc3f5a655c90e08415
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-15 12:48:04 +0300

    here is a func. whose name is draggable

--- src/components/TrelloCard.js
+++ src/components/TrelloCard.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { Card, makeStyles, Typography } from "@material-ui/core";
 import { CardContent } from "@material-ui/core";
+import { Draggable } from "react-beautiful-dnd";
 
-
-const useStyles = makeStyles ({
-    cardsBox: {
-        marginBottom: 8,
-    }
+const useStyles = makeStyles({
+  cardsBox: {
+    marginBottom: 8,
+  },
 });
 
-
-
-const TrelloCard = ({ text }) => {
-
-    const classes = useStyles();
+const TrelloCard = ({ text, id, index }) => {
+  const classes = useStyles();
 
   return (
-    <div>
-      <Card className={classes.cardsBox}>
-        <CardContent>
-          <Typography gutterBottom>{text}</Typography>
-        </CardContent>
-      </Card>
-    </div>
+    <Draggable draggableId={String(id)} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <Card className={classes.cardsBox}>
+            <CardContent>
+              <Typography gutterBottom>{text}</Typography>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
